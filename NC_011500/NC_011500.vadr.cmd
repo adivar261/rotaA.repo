fd7cc396a9e7388364815fe4ec302d7dad6a54b3
Adithi <adithiv261@gmail.com>
2024-06-27 11:19:24 -0400

    Initial project version

---
+++ NC_011500/NC_011500.vadr.cmd
@@ -0,0 +1,23 @@
+mkdir NC_011500
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/esl-reformat --informat afa  stockholm NC_011500/NC_011500.vadr.fa > NC_011500/NC_011500.vadr.stk
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/esl-reformat --informat fasta -d -u fasta NC_011500/NC_011500.vadr.fa > NC_011500/NC_011500.vadr.fa.tmp
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/ncbi-blast/bin/makeblastdb -in NC_011500/NC_011500.vadr.fa -dbtype nucl > /dev/null
+rm NC_011500/NC_011500.vadr.fa.tmp
+rm NC_011500/NC_011500.vadr.fa.tmp.ssi
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/esl-sfetch --index NC_011500/NC_011500.vadr.fa > /dev/null
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/esl-translate  -M -l 3 --watson NC_011500/NC_011500.vadr.cds.fa > NC_011500/NC_011500.vadr.cds.esl-translate.1.fa
+rm NC_011500/NC_011500.vadr.cds.esl-translate.1.fa
+rm NC_011500/NC_011500.vadr.cds.esl-translate.2.fa
+rm NC_011500/NC_011500.vadr.cds.esl-translate.2.fa.ssi
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/ncbi-blast/bin/makeblastdb -in NC_011500/NC_011500.vadr.protein.fa -dbtype prot > /dev/null
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/esl-sfetch NC_011500/NC_011500.vadr.protein.fa NC_011500.2/31..1521:+ | /net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/hmmbuild -n NC_011500/31..1521:+ --informat afa NC_011500/NC_011500.vadr.1.hmm - > NC_011500/NC_011500.vadr.1.hmmbuild
+cat NC_011500/NC_011500.vadr.1.hmm > NC_011500/NC_011500.vadr.protein.hmm
+rm  NC_011500/NC_011500.vadr.1.hmm
+cat NC_011500/NC_011500.vadr.1.hmmbuild > NC_011500/NC_011500.vadr.protein.hmmbuild
+rm  NC_011500/NC_011500.vadr.1.hmmbuild
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/hmmpress NC_011500/NC_011500.vadr.protein.hmm > NC_011500/NC_011500.vadr.hmmpress
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/cmbuild -n NC_011500 --verbose  --noss --noh3pri NC_011500/NC_011500.vadr.cm NC_011500/NC_011500.vadr.stk > NC_011500/NC_011500.vadr.cmbuild
+/net/intdev/oblast01/dnaorg/virseqannot/code/vadr-install-1.6.3/infernal/binaries/cmpress NC_011500/NC_011500.vadr.cm > NC_011500/NC_011500.vadr.cmpress
+# Thu May 30 10:48:36 EDT 2024
+# Linux cbbdev12 4.18.0-513.24.1.el8_9.x86_64 #1 SMP Mon Apr 8 11:23:13 EDT 2024 x86_64 x86_64 x86_64 GNU/Linux
+[ok]
---
+++ NC_011500/NC_011500.vadr.fa.njs
@@ -0,0 +1,22 @@
+{
+  "version": "1.2",
+  "dbname": "NC_011500.vadr.fa",
+  "dbtype": "Nucleotide",
+  "db-version": 5,
+  "description": "NC_011500/NC_011500.vadr.fa",
+  "number-of-letters": 1614,
+  "number-of-sequences": 1,
+  "last-updated": "2024-05-30T10:48:00",
+  "number-of-volumes": 1,
+  "bytes-total": 37505,
+  "bytes-to-cache": 541,
+  "files": [
+    "NC_011500.vadr.fa.ndb",
+    "NC_011500.vadr.fa.nhr",
+    "NC_011500.vadr.fa.nin",
+    "NC_011500.vadr.fa.not",
+    "NC_011500.vadr.fa.nsq",
+    "NC_011500.vadr.fa.ntf",
+    "NC_011500.vadr.fa.nto"
+  ]
+}
